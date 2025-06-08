bad0f5213e35e804af5107cd3fa5633e0a98ee39
Ritik Chourasiya <ritvikchoure65+git@gmail.com>
2025-06-08 09:43:03 +0530

    feat: oauth implementation

---
+++ src/config/oauth.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        "/api/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // TODO: Find or create user in your DB
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj, done) => done(null, obj));
--- src/server.js
+++ src/server.js
@@ -6,11 +6,29 @@ const path = require("path");
 const morgan = require("morgan");
 const cors = require("cors");
 
+const session = require("express-session");
+const passport = require("passport");
+
 const mongodb = require("./config/database.js");
 const apiRoutes = require("./routes/index.route.js");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.use(
+  session({
+    secret: "hello-pingtohr",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: true,
+      sameSite: "none",
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Middleware
 app.use(morgan("dev"));
@@ -22,6 +40,29 @@ app.use(cors());
 app.use("/uploads", express.static("uploads")); // to serve resume files
 
 app.use("/api", apiRoutes);
+
+
+// Start OAuth flow
+app.get('/api/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+// Callback URL
+app.get('/api/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/login',
+    session: true
+  }),
+  (req, res) => {
+    // On success, redirect to React app
+    res.redirect('/dashboard');
+  }
+);
+
+// Get logged-in user
+app.get('/api/auth/me', (req, res) => {
+  res.json(req.user || null);
+});
 
 app.listen(PORT, () => {
   console.log(`Email tracking server running at http://localhost:${PORT}`);
